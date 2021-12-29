41fb5c83af5e6f31e9e7d4a26b0c1013850ab41c
Your Name <robertcjohnson1984@gmail.com>
2021-12-29 14:51:01 -0700

    add auth provider and react-router-dom routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,49 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+import Signup from './pages/Signup';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-  
-  )
+    <ApolloProvider client={client}>
+    <Router>
+      <div>
+        <Provider store={store}>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/signup" component={Signup} />
+          </Switch>
+        </Provider>
+      </div>
+    </Router>
+  </ApolloProvider>
+  );
 }
 
 export default App;
