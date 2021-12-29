aa14cc6327b50b8c403bc3b95d24d822bf2ebc79
Your Name <robertcjohnson1984@gmail.com>
2021-12-29 15:34:17 -0700

    create order history page for customers

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,7 @@ import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Nav from './components/Nav';
 import Success from './pages/Success';
+import OrderHistory from './pages/OrderHistory';
 
 const httpLink = createHttpLink({
   uri: '/graphql',
@@ -47,6 +48,7 @@ function App() {
             <Route exact path="/login" component={Login} />
             <Route exact path="/signup" component={Signup} />
             <Route exact path="/success" component={Success} />
+            <Route exact path="/orderHistory" component={OrderHistory} />
             <Route exact path="/products/:id" component={Detail} />
             <Route component={NoMatch} />
           </Switch>
