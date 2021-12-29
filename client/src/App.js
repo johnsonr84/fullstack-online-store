426940477bf8a28a9a89691c19dfc217c1848ee5
Your Name <robertcjohnson1984@gmail.com>
2021-12-29 15:30:52 -0700

    import success page

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ import Detail from './pages/Detail';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Nav from './components/Nav';
+import Success from './pages/Success';
 
 const httpLink = createHttpLink({
   uri: '/graphql',
@@ -44,6 +45,7 @@ function App() {
             <Route exact path="/" component={Home} />
             <Route exact path="/login" component={Login} />
             <Route exact path="/signup" component={Signup} />
+            <Route exact path="/success" component={Success} />
             <Route exact path="/products/:id" component={Detail} />
           </Switch>
         </Provider>
